d34887d94da7b82d4e075b787cb0124dffb04931
yoana-y <yoana.yosifovva@gmail.com>
2025-04-24 23:53:38 +0300

    exam solutions

---
+++ Exam/01.Problem/Program.cs
@@ -0,0 +1,55 @@
+﻿//using System;
+
+//class Program
+//{
+//    static void Main()
+//    {
+//        int N = int.Parse(Console.ReadLine());
+
+//        if (N <= 0)
+//        {
+//            Console.WriteLine(0);
+//            return;
+//        }
+
+//        int cumulativeSum = 0;
+//        for (int i = 0; i < N; i++)
+//        {
+//            int dailyIntake = int.Parse(Console.ReadLine());
+//            if (dailyIntake < 0)
+//            {
+//                dailyIntake = 0;
+//            }
+//            cumulativeSum += dailyIntake;
+//            Console.WriteLine(cumulativeSum);
+//        }
+//    }
+//}
+
+namespace _01.Problem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberDays = int.Parse(Console.ReadLine());
+            int totalWaterConsume = 0;
+
+
+            for (int i = 0; i < numberDays; i++)
+            {
+                int newWaterConsume = int.Parse(Console.ReadLine());
+
+                if (newWaterConsume <= 0)
+                {
+                    Console.WriteLine("0");
+                    continue;
+                }
+                totalWaterConsume += newWaterConsume;
+                Console.WriteLine(totalWaterConsume);
+            }
+
+        }
+    }
+}
+
---
+++ Exam/02.Problem/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] dailyAmounts = Console.ReadLine()
+                                   .Split(" ")
+                                   .Select(int.Parse)
+                                   .ToArray();
+
+        int startDay = int.Parse(Console.ReadLine());
+        int endDay = int.Parse(Console.ReadLine());
+
+        int sumBeforeDivision = 0;
+
+        double sumRain = 0;        
+        double counter = 0.00;
+
+        for (int i = startDay; i <= endDay; i++)
+        {
+            int dailyRain = dailyAmounts[i];
+
+            sumBeforeDivision += dailyRain;
+
+            sumRain += dailyRain;
+
+            counter++;
+
+        }
+
+        sumRain /= counter;
+
+        Console.WriteLine(sumBeforeDivision);
+
+        Console.WriteLine($"{sumRain:f2}");
+
+    }
+}
