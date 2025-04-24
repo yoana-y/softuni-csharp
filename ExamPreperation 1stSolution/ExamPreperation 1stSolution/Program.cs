dac9fbabb907cf57c0235fe62643f04c7a7742a4
yoana-y <yoana.yosifovva@gmail.com>
2025-04-24 23:41:14 +0300

    exam preparation 1

---
+++ ExamPreperation 1stSolution/ExamPreperation 1stSolution/Program.cs
@@ -0,0 +1,60 @@
+﻿
+int number = int.Parse(Console.ReadLine());
+
+int sum = 0;
+
+while (number > 0)
+{
+    int lastDigit = number % 10;
+
+    if (lastDigit % 2 == 0)
+    {
+        int currentFactorial = CheckFactoriel(lastDigit);
+        sum += currentFactorial;
+    }
+
+    number = number / 10;
+}
+
+Console.WriteLine(sum);
+static int CheckFactoriel(int num)
+{
+    int factorial = 1;
+
+    for (int i = 1; i <= num; i++)
+    {
+        factorial *= i;
+    }
+
+    return factorial;
+}
+
+
+//Second Solution below:
+
+//string number = Console.ReadLine();
+
+//int sum = 0;
+
+//for (int i = 0; i < number.Length; i++)
+//{
+//    int lastDigit = int.Parse(number[i].ToString());
+
+//    if (lastDigit / 2 == 0)
+//    {
+//        int currentFactorial = FactorialNumber(lastDigit);
+//        sum = sum + currentFactorial;
+//    }
+//}
+
+//Console.WriteLine(sum);
+
+//static int FactorialNumber(int n)
+//{
+//    if (n == 0 || n == 1)
+//    {
+//        return 1;
+//    }
+
+//    return n * FactorialNumber(n - 1);
+//}
